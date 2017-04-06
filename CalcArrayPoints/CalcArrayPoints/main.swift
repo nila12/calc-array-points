2f1d4830b0f7c96df5a134479c7212f16e8e2ef1
Mathi Manavalan <mathimano@hotmail.com>
2017-04-06 15:08:06 -0700

    instructions

--- CalcArrayPoints/CalcArrayPoints/main.swift
+++ CalcArrayPoints/CalcArrayPoints/main.swift
@@ -10,3 +10,34 @@ import Foundation
 
 print("Hello, World!")
 
+/* 
+create 4 functions for add, subtract, multiply, divide
+create a function that expects one of them along with 2 ints; aka a generic math operation function, it should take 2 ints and pass them to the function and return the result
+*/
+
+/*
+create two new functions for add/multiply
+    instead of taking 2 ints, take an array of ints; return result of (adding/multiplying) all of the elements
+ 
+ create two new functions (count, avg) that take an array of ints; operate on the array and return the result
+ 
+ refactor all four down to another generic math operation approach (like in previous part)
+ */
+
+
+/*
+ use tuples to represent x/y pairs
+ 
+ create 2 functions for adding and subtracting points: (1,2) + (3,4) = (4,6)
+    (1,2)-(3,4)=(-2,-2)
+ */
+
+/*
+ now use Dictionaries to represent points; "x" is x coor, "y" is y coor
+ 
+ create 2 functions for adding and subtracting points
+ 
+ handle dictionaries without "x" or "y" without crashing
+ 
+ handle dictionaries with Double values without crashing
+ */
