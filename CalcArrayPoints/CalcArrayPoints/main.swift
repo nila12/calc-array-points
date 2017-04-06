f64a8dc5b6e4585f6b8bd50d44e900d68ba82ae9
Mathi Manavalan <mathimano@hotmail.com>
2017-04-06 15:57:54 -0700

    done array fun

--- CalcArrayPoints/CalcArrayPoints/main.swift
+++ CalcArrayPoints/CalcArrayPoints/main.swift
@@ -15,37 +15,29 @@ create 4 functions for add, subtract, multiply, divide
 create a function that expects one of them along with 2 ints; aka a generic math operation function, it should take 2 ints and pass them to the function and return the result
 */
 
-func add(firstNum first: Int, secondNum sec: Int) {// -> Int {
+func add(firstNum first: Int, secondNum sec: Int) -> Int {
     
-    print ("\(first) + \(sec) = \(first + sec)")
-    
-    //return 1;
+    return (first + sec)
 }
 
-func subtract(firstNum first: Int, secondNum sec: Int) {// -> Int {
+func subtract(firstNum first: Int, secondNum sec: Int) -> Int {
     
-    print ("\(first) - \(sec) = \(first - sec)")
-    
-    //return 1;
+    return (first - sec)
 }
 
-func multiply(firstNum first: Int, secondNum sec: Int) {// -> Int {
+func multiply(firstNum first: Int, secondNum sec: Int) -> Int {
     
-    print ("\(first) * \(sec) = \(first * sec)")
-    
-    //return 1;
+    return (first * sec)
 }
 
-func divide(firstNum first: Int, secondNum sec: Int) {// -> Int {
+func divide(firstNum first: Int, secondNum sec: Int) -> Int {
     
-    print ("\(first) / \(sec) = \(first / sec)")
-    
-    //return 1;
+    return (first / sec)
 }
 
-func basicOperation(firstNum first: Int, secondNum sec: Int, operation: (Int, Int) -> Void) {
+func basicOperation(firstNum first: Int, secondNum sec: Int, operation: (Int, Int) -> Int) -> Int {
     
-    print(operation(first, sec))
+    return(operation(first, sec)) //just number or "result" text???
 }
 
 
@@ -58,7 +50,45 @@ create two new functions for add/multiply
  refactor all four down to another generic math operation approach (like in previous part)
  */
 
+func addArr(arr: [Int]) -> Int {
+    var sum = 0;
+    
+    for num in arr {
+        sum += num;
+    }
+    
+    return sum
+}
 
+func multiplyArr(arr: [Int]) -> Int {
+    var sum = 1;
+    
+    for num in arr {
+        sum *= num;
+    }
+    
+    return sum
+}
+
+func count(arr: [Int]) -> Int {
+    return arr.count
+}
+
+func avg(arr: [Int]) -> Int { //can i use my addArr func???
+    var sum = 0;
+    
+    for num in arr {
+        sum += num;
+    }
+    
+    return sum / arr.count
+}
+
+func arrOperation(arr: [Int], arrOperation: ([Int]) -> Int) -> Int {
+    
+    return arrOperation(arr)
+    
+}
 
 
 /*
@@ -67,6 +97,9 @@ create two new functions for add/multiply
  create 2 functions for adding and subtracting points: (1,2) + (3,4) = (4,6)
     (1,2)-(3,4)=(-2,-2)
  */
+
+
+
 
 /*
  now use Dictionaries to represent points; "x" is x coor, "y" is y coor
